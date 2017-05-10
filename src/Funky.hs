bb48fb5607082eb2ceb3e6a4f7d28e3c3f62f587
Thiago Campezzi <Thiago.ViccinoCampezzi@myob.com>
2017-05-10 17:55:49 +1000

    Add Functor exercises

---
+++ src/Funky.hs
@@ -0,0 +1,109 @@
+module Funky where
+
+-- functions to prove functor laws
+verifyIdentity
+  :: (Functor f, Eq (f a))
+  => f a -> (Bool, f a)
+verifyIdentity f = (valid, result)
+  where
+    result = fmap id f
+    valid = f == result
+
+verifyComposition
+  :: (Num a, Functor f, Eq (f a))
+  => f a -> (Bool, f a, f a)
+verifyComposition x = (valid, resultA, resultB)
+  where
+    f = (+ 1)
+    g = (* 2)
+    resultA = (fmap g (fmap f x))
+    resultB = (fmap (g . f) x)
+    valid = resultA == resultB
+
+-- Identity functor
+newtype Identity a =
+  Identity a
+  deriving (Eq, Show)
+
+instance Functor Identity where
+  fmap f (Identity a) = Identity (f a)
+
+-- Pair functor
+data Pair a =
+  Pair a
+       a
+  deriving (Eq, Show)
+
+instance Functor Pair where
+  fmap f (Pair a b) = Pair (f a) (f b)
+
+-- Two functor
+data Two a b =
+  Two a
+      b
+  deriving (Eq, Show)
+
+instance Functor (Two a) where
+  fmap f (Two a b) = Two a (f b)
+
+-- Three functor
+data Three a b c =
+  Three a
+        b
+        c
+  deriving (Eq, Show)
+
+instance Functor (Three a b) where
+  fmap f (Three a b c) = Three a b (f c)
+
+-- Three' functor
+data Three' a b =
+  Three' a
+         b
+         b
+  deriving (Eq, Show)
+
+instance Functor (Three' a) where
+  fmap f (Three' a b c) = Three' a (f b) (f c)
+
+-- Four functor
+data Four a b c d =
+  Four a
+       b
+       c
+       d
+  deriving (Eq, Show)
+
+instance Functor (Four a b c) where
+  fmap f (Four a b c d) = Four a b c (f d)
+
+-- Four' functor
+data Four' a b =
+  Four' a
+        a
+        a
+        b
+  deriving (Eq, Show)
+
+instance Functor (Four' a) where
+  fmap f (Four' a b c d) = Four' a b c (f d)
+
+-- output
+verifyAll :: IO ()
+verifyAll = do
+  verifyFunctor (Identity 1 :: Identity Integer) "Identity"
+  verifyFunctor (Pair 1 1 :: Pair Integer) "Pair"
+  verifyFunctor (Two 0 1 :: Two Integer Integer) "Two"
+  verifyFunctor (Three 0 0 1 :: Three Integer Integer Integer) "Three"
+  verifyFunctor (Three' 0 1 1 :: Three' Integer Integer) "Three'"
+  verifyFunctor (Four 0 0 0 1 :: Four Integer Integer Integer Integer) "Four"
+  verifyFunctor (Four' 0 0 0 1 :: Four' Integer Integer) "Four"
+
+verifyFunctor
+  :: (Functor f, Num a, Show (f a), Eq (f a))
+  => f a -> String -> IO ()
+verifyFunctor f name = do
+  putStrLn $ "--- " ++ name ++ " ---"
+  putStrLn $ "identity: " ++ (show $ verifyIdentity f)
+  putStrLn $ "composition: " ++ (show $ verifyComposition f)
+  putStrLn ""
