65ab54e07207e65d0be0877a3c516e699e4102e4
Thiago Campezzi <Thiago.ViccinoCampezzi@myob.com>
2017-05-23 14:50:58 +1000

    Starting monad exercise

--- src/MonadPreamble.hs
+++ src/MonadPreamble.hs
@@ -69,3 +69,21 @@ mkCow name' age' weight' = do
   a <- noNegative age'
   w <- noNegative weight'
   weightCheck (Cow n a w)
+
+--
+data Sumthing a b
+  = First a
+  | Second b
+  deriving (Eq, Show)
+
+instance Functor (Sumthing a) where
+  fmap _ (First a) = First a
+  fmap f (Second b) = Second (f b)
+
+instance Applicative (Sumthing a) where
+  pure = undefined
+  (<*>) = undefined
+
+instance Monad (Sumthing a) where
+  return = pure
+  (>>=) = undefined
