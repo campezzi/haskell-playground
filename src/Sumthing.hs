e44e1a7296692942f559ef07788f57ec1264644f
Thiago Campezzi <campezzi@gmail.com>
2017-05-24 21:31:40 +1000

    Look a monad!

--- src/Sumthing.hs
+++ src/Sumthing.hs
@@ -1,5 +1,9 @@
 module Sumthing where
 
+import Test.QuickCheck
+import Test.QuickCheck.Checkers
+import Test.QuickCheck.Classes
+
 data Sumthing a b
   = First a
   | Second b
@@ -10,9 +14,29 @@ instance Functor (Sumthing a) where
   fmap f (Second b) = Second (f b)
 
 instance Applicative (Sumthing a) where
-  pure = undefined
-  (<*>) = undefined
+  pure = Second
+  First x <*> _ = First x
+  _ <*> First x = First x
+  Second f <*> Second x = Second (f x)
 
 instance Monad (Sumthing a) where
   return = pure
-  (>>=) = undefined
+  First x >>= _ = First x
+  Second x >>= f = f x
+
+instance (Arbitrary a, Arbitrary b) => Arbitrary (Sumthing a b) where
+  arbitrary = do
+    x <- arbitrary
+    y <- arbitrary
+    elements [First x, Second y]
+
+instance (Eq a, Eq b) => EqProp (Sumthing a b) where
+  (=-=) = eq
+
+checkSumthing :: IO ()
+checkSumthing = do
+  quickBatch $ functor trigger
+  quickBatch $ applicative trigger
+  quickBatch $ monad trigger
+  where
+    trigger = undefined :: Sumthing Int (Int, Bool, String)
