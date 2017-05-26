37d75c1e09ba42dbd7e341a0507dbc774e47362e
Thiago Campezzi <Thiago.ViccinoCampezzi@myob.com>
2017-05-26 13:26:40 +1000

    Identity monad exercise

--- src/MonadExercises.hs
+++ src/MonadExercises.hs
@@ -31,3 +31,35 @@ checkNope = do
   quickBatch $ monad trigger
   where
     trigger = undefined :: Nope (Int, Int, Int)
+
+--
+newtype MIdentity a =
+  MIdentity a
+  deriving (Eq, Show)
+
+instance Functor MIdentity where
+  fmap f (MIdentity a) = MIdentity $ f a
+
+instance Applicative MIdentity where
+  pure = MIdentity
+  (MIdentity f) <*> (MIdentity x) = MIdentity $ f x
+
+instance Monad MIdentity where
+  return = pure
+  (MIdentity x) >>= f = f x
+
+instance (Arbitrary a) => Arbitrary (MIdentity a) where
+  arbitrary = do
+    x <- arbitrary
+    return $ MIdentity x
+
+instance (Eq a) => EqProp (MIdentity a) where
+  (=-=) = eq
+
+checkMIdentity :: IO ()
+checkMIdentity = do
+  quickBatch $ functor trigger
+  quickBatch $ applicative trigger
+  quickBatch $ monad trigger
+  where
+    trigger = undefined :: MIdentity (Int, Int, Int)
