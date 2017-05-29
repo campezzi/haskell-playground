3f709e5a40514d2fdd54aa6196f7541713f7a80d
Thiago Campezzi <Thiago.ViccinoCampezzi@myob.com>
2017-05-29 13:13:12 +1000

    Start Reader madness

---
+++ src/ReaderPreamble.hs
@@ -0,0 +1,27 @@
+module ReaderPreamble where
+
+import Control.Applicative
+
+boop :: Integer -> Integer
+boop = (* 2)
+
+doop :: Integer -> Integer
+doop = (+ 10)
+
+bip :: Integer -> Integer
+bip = boop . doop
+
+bloop :: Integer -> Integer
+bloop = fmap boop doop
+
+bbop :: Integer -> Integer
+bbop = (+) <$> boop <*> doop
+
+duwop :: Integer -> Integer
+duwop = liftA2 (+) boop doop
+
+boopDoop :: Integer -> Integer
+boopDoop = do
+  a <- boop
+  b <- doop
+  return (a + b)
