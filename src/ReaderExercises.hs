83f4641ee86912b294b355de39771fd280a8f798
Thiago Campezzi <campezzi@gmail.com>
2017-05-29 21:42:49 +1000

    Man down

--- src/ReaderExercises.hs
+++ src/ReaderExercises.hs
@@ -1,6 +1,7 @@
 module ReaderExercises where
 
 import Control.Applicative (liftA2)
+import Control.Monad (join, liftM2)
 import Data.Char
 
 cap :: [Char] -> [Char]
@@ -67,6 +68,12 @@ getDogR = Dog <$> dogName <*> address
 getDogR' :: Person -> Dog
 getDogR' = liftA2 Dog dogName address
 
+getDogRM :: Person -> Dog
+getDogRM = do
+  name <- dogName
+  addy <- address
+  return $ Dog name addy
+
 --
 myLiftA2 :: Applicative f => (a -> b -> c) -> f a -> f b -> f c
 myLiftA2 f x y = f <$> x <*> y
@@ -76,9 +83,19 @@ newtype Reader r a = Reader
   { runReader :: r -> a
   }
 
+-- fmap: (a -> b) -> Reader (r -> a) -> Reader (r -> b)
 instance Functor (Reader r) where
   fmap f (Reader a) = Reader $ f . a
 
+-- <*>: Reader (r -> a -> b) -> Reader (r -> a) -> Reader (r -> b)
 instance Applicative (Reader r) where
   pure a = Reader $ \_ -> a
   (Reader fab) <*> (Reader fa) = Reader $ \r -> fab r (fa r)
+
+-- >>=: Reader (r -> a) -> (a -> Reader (r -> b)) -> Reader (r -> b)
+instance Monad (Reader r) where
+  return = pure
+  (Reader ra) >>= arb = join (Reader $ arb . ra)
+
+getDogRM' :: Reader Person Dog
+getDogRM' = Reader $ liftM2 Dog dogName address
