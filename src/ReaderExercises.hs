fcb448a4a4ab209c0a8f8a5e5d075f5f1e440840
Thiago Campezzi <Thiago.ViccinoCampezzi@myob.com>
2017-05-29 13:37:57 +1000

    Why is this happening to me

--- src/ReaderExercises.hs
+++ src/ReaderExercises.hs
@@ -1,1 +1,28 @@
 module ReaderExercises where
+
+import Control.Applicative (liftA2)
+import Data.Char
+
+cap :: [Char] -> [Char]
+cap = fmap toUpper
+
+rev :: [Char] -> [Char]
+rev = reverse
+
+composed :: [Char] -> [Char]
+composed = reverse . cap
+
+fmapped :: [Char] -> [Char]
+fmapped = reverse <$> cap
+
+tupled :: [Char] -> ([Char], [Char])
+tupled = liftA2 (,) cap rev
+
+tupledM :: [Char] -> ([Char], [Char])
+tupledM = do
+  x <- cap
+  y <- rev
+  return (x, y)
+
+tupledM' :: [Char] -> ([Char], [Char])
+tupledM' = cap >>= (\x -> rev >>= (\y -> return (x, y)))
