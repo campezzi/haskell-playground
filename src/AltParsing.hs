8951040ff80c1e1eb09470b7ca602c13f73f12ed
Thiago Campezzi <campezzi@gmail.com>
2017-06-05 23:52:01 +1000

    Parse Fractions or Decimal

--- src/AltParsing.hs
+++ src/AltParsing.hs
@@ -1,9 +1,21 @@
+{-# LANGUAGE QuasiQuotes #-}
+
 module AltParsing where
 
 import Control.Applicative
+import Data.Ratio ((%))
+import Text.RawString.QQ
 import Text.Trifecta
 
 type NumberOrString = Either Integer String
+
+eitherOr :: String
+eitherOr =
+  [r|
+123
+abc
+456
+def|]
 
 a :: String
 a = "blah"
@@ -15,14 +27,40 @@ c :: String
 c = "123blah789"
 
 parseNoS :: Parser NumberOrString
-parseNoS = (Left <$> integer) <|> (Right <$> some letter)
+parseNoS =
+  skipMany (oneOf "\n") >> (Left <$> integer) <|> (Right <$> some letter)
 
 testAlt :: IO ()
 testAlt = do
-  print $ parseString (some letter) mempty a
-  print $ parseString integer mempty b
-  print $ parseString parseNoS mempty a
-  print $ parseString parseNoS mempty b
-  print $ parseString parseNoS mempty c
-  print $ parseString (many parseNoS) mempty c
-  print $ parseString (some parseNoS) mempty c
+  print $ parseString (some parseNoS) mempty eitherOr
+
+--
+data DecimalOrFraction
+  = Decimal Double
+  | Fraction Rational
+  deriving (Eq, Show)
+
+parseDecimal :: Parser Double
+parseDecimal = do
+  integerPart <- integer
+  char '.'
+  decimalPart <- integer
+  return $ toDouble integerPart decimalPart
+
+toDouble :: Integer -> Integer -> Double
+toDouble i d = (fromInteger ((i * multiplier) + d)) / (fromInteger multiplier)
+  where
+    multiplier = (10 ^) . length . show $ d
+
+parseFraction :: Parser Rational
+parseFraction = do
+  numerator <- decimal
+  char '/'
+  denominator <- decimal
+  case denominator of
+    0 -> fail "Denominator cannot be zero"
+    _ -> return (numerator % denominator)
+
+parseDecimalOrFraction :: Parser DecimalOrFraction
+parseDecimalOrFraction =
+  try (Decimal <$> parseDecimal) <|> (Fraction <$> parseFraction)
