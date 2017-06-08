e2747f78d1c22f33daa1104c79eaeb14b080cdcd
Thiago Campezzi <Thiago.ViccinoCampezzi@myob.com>
2017-06-09 08:43:36 +1000

    Either transformer

--- src/TransformersPreamble.hs
+++ src/TransformersPreamble.hs
@@ -26,3 +26,41 @@ instance (Monad m) => Monad (MaybeT m) where
       case v of
         Nothing -> return Nothing
         Just x -> runMaybeT (f x)
+
+--
+newtype EitherT e m a = EitherT
+  { runEitherT :: m (Either e a)
+  }
+
+instance (Show (m (Either e a))) => Show (EitherT e m a) where
+  show (EitherT mea) = "EitherT " ++ show mea
+
+instance (Functor m) => Functor (EitherT e m) where
+  fmap f (EitherT mea) = EitherT $ (fmap . fmap) f mea
+
+instance (Applicative m) => Applicative (EitherT e m) where
+  pure a = EitherT $ (pure . pure) a
+  (EitherT meab) <*> (EitherT mea) = EitherT $ liftA2 (<*>) meab mea
+
+instance (Monad m) => Monad (EitherT e m) where
+  return = pure
+  (EitherT mea) >>= f =
+    EitherT $ do
+      v <- mea
+      case v of
+        Left a -> return $ Left a
+        Right b -> runEitherT (f b)
+
+swapEither :: Either e a -> Either a e
+swapEither (Left x) = Right x
+swapEither (Right x) = Left x
+
+swapEitherT :: (Functor m) => EitherT e m a -> EitherT a m e
+swapEitherT (EitherT mea) = EitherT $ fmap swapEither mea
+
+eitherT :: (Monad m) => (e -> m c) -> (a -> m c) -> EitherT e m a -> m c
+eitherT femc famc (EitherT mea) = do
+  v <- mea
+  case v of
+    Left e -> femc e
+    Right a -> famc a
