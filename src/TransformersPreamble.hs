b17d66d8476a32bd77e2211b556ce0827a3ad534
Thiago Campezzi <campezzi@gmail.com>
2017-06-10 01:35:48 +1000

    State Transformer instances

--- src/TransformersPreamble.hs
+++ src/TransformersPreamble.hs
@@ -95,3 +95,29 @@ instance (Monad m) => Monad (ReaderT r m) where
     ReaderT $ \r -> do
       a <- rma r
       runReaderT (f a) r
+
+--
+newtype StateT s m a = StateT
+  { runStateT :: s -> m (a, s)
+  }
+
+instance (Functor m) => Functor (StateT s m) where
+  fmap f (StateT smas) =
+    StateT $ \s ->
+      let mas = smas s
+      in fmap (\(a, s') -> (f a, s')) mas
+
+instance (Monad m) => Applicative (StateT s m) where
+  pure x = StateT $ \s -> pure (x, s)
+  (StateT smfabs) <*> (StateT smas) =
+    StateT $ \s -> do
+      (fab, s') <- smfabs s
+      (a, s'') <- smas s'
+      return (fab a, s'')
+
+instance (Monad m) => Monad (StateT s m) where
+  return = pure
+  (StateT smas) >>= f =
+    StateT $ \s -> do
+      (a, s') <- smas s
+      runStateT (f a) s'
