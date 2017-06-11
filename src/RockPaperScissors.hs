796944185acc8a06b699a244c1f4353cdda8afe5
Thiago Campezzi <campezzi@gmail.com>
2017-06-11 13:34:00 +1000

    Finish RockPaperScissors

--- src/RockPaperScissors.hs
+++ src/RockPaperScissors.hs
@@ -1,12 +1,14 @@
 module RockPaperScissors where
 
+import Control.Monad (guard, replicateM)
+import Control.Monad.Trans.State
 import System.Random
 
 data Choice
   = Rock
   | Paper
   | Scissors
-  deriving (Eq, Show, Ord)
+  deriving (Eq, Show, Ord, Read)
 
 instance Random Choice where
   random g = (randomChoice, nextGen)
@@ -18,15 +20,20 @@ instance Random Choice where
 
 data Outcome
   = Win
-  | Draw
   | Lose
+  | Draw
   deriving (Eq, Show)
 
-data Game = Game
-  { myChoice :: Maybe Choice
-  , opponentChoice :: Maybe Choice
-  , outcome :: Maybe Outcome
+data Scoreboard = Scoreboard
+  { playerScore :: Int
+  , opponentScore :: Int
   }
+
+instance Show Scoreboard where
+  show (Scoreboard p o) = "Player " ++ (show p) ++ ", Opponent " ++ (show o)
+
+newScoreboard :: Scoreboard
+newScoreboard = Scoreboard 0 0
 
 toChoice :: Int -> Choice
 toChoice a =
@@ -46,8 +53,32 @@ calculateOutcome opponent player
   | opponent == defeatedBy player = Win
   | otherwise = Lose
 
-play :: IO ()
-play = undefined
--- generate opponent choice
--- ask player for his choice
--- calculate outcome
+updateScoreboard :: Scoreboard -> Outcome -> Scoreboard
+updateScoreboard (Scoreboard player opponent) outcome =
+  case outcome of
+    Win -> Scoreboard (player + 1) opponent
+    Lose -> Scoreboard player (opponent + 1)
+    _ -> Scoreboard player opponent
+
+friendly :: Outcome -> String
+friendly o =
+  case o of
+    Win -> "You win!"
+    Lose -> "You lose!"
+    Draw -> "It's a draw!"
+
+playRound :: StateT Scoreboard IO Outcome
+playRound =
+  StateT $ \s -> do
+    opponent <- randomIO :: IO Choice
+    putStrLn "Type your choice!"
+    player <- fmap read getLine
+    let outcome = calculateOutcome opponent player
+    putStrLn $
+      "Your opponent picked " ++ (show opponent) ++ ". " ++ (friendly outcome)
+    return (outcome, updateScoreboard s outcome)
+
+newGame :: Int -> IO ()
+newGame rounds = do
+  (_, s) <- runStateT (replicateM rounds playRound) newScoreboard
+  putStrLn $ "Game over! Final score: " ++ show s
